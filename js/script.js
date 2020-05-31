cef02c601bc2abe8d9e629e4f30ec7faac56b975
Md Zaman <md83shahnewaz@gmail.com>
2020-05-31 14:31:44 -0400

    Refactor some folder and code

--- js/script.js
+++ js/script.js
@@ -12,16 +12,28 @@ function inputLength(){
 }
 
 function createListElement(){
+
+    
+
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ol.appendChild(li)
     input.value = "";
 
-    //======
+
+    //Create checkbox
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox"; 
+    checkbox.name = "slct[]";
+    checkbox.value = ""; 
+    checkbox.appendChild(document.createTextNode(checkbox.value))
+    li.appendChild(checkbox);
+ 
+    //====Create delete button
     var btn = document.createElement("button");
     btn.appendChild(document.createTextNode("D"));
     li.appendChild(btn);
- 	btn.onclick = removeParent;
+    btn.onclick = removeParent;
 }
 
 //3rd Item
@@ -39,70 +51,21 @@ function addListAfterkeyPress(event){
 }
 input.addEventListener("keypress", addListAfterkeyPress);
 
-//==========
+//===Strickthrough on the item
 ol.onclick=function(event){
-	var target=event.target;
-	target.classList.toggle("done");
+    var target=event.target;
+    target.classList.toggle("done");
 }
-
-// function listLength(){
-// 	return li.length;
-// }
-
+//==Delete the item
 function deleteButton(){
-	// var btn=document.createElement("button");
-    // btn.appendChild(document.createTextNode("Delete!"));
-    // li[i].appendChild(btn);
     btn.onclick=removeParent;
 }
-
-// for( i=0;i<listLength();i++){
-//     deleteButton();
-// }
-
 function removeParent(event){
 	event.target.parentNode.remove();
 }
-
-
-//2nd Attempt
-// buttonEnter.addEventListener("click", function(){   
-//     if(inputLength() > 0){
-//         createListElement();    
-//     }   
-// })
-
-// input.addEventListener("keypress", function(event){   
-//     if(inputLength() > 0 && event.keyCode==13){
-//         createListElement(); 
-//     }   
-// })
-
-
-
-//Step by Step from cratch First Attempt 
-// //add eventListner
-// buttonEnter.addEventListener("click", function(){   
-//         //adding list with only typing
-//         if(input.value.length > 0){
-//             //adding list including not typing
-//             var li = document.createElement("li");
-//             li.appendChild(document.createTextNode(input.value));
-//             ul.appendChild(li)
-//             //after clicking I want input box is automatically empty
-//             input.value = "";
-            
-
-//         }   
-//  })
-
-//  //Keboard enter event
-//  input.addEventListener("keypress", function(){   
-//      //Keboard enter event
-//     if(input.value.length > 0 && event.keyCode==13){
-//         var li = document.createElement("li");
-//         li.appendChild(document.createTextNode(input.value));
-//         ul.appendChild(li)
-//         input.value = "";
-//     }   
-// });
+//==Check and Strickthrough on the item
+function updateItem(){
+    if (checkbox.checked){
+      checkbox.style.textDecoration="line-through";
+    }
+  }
